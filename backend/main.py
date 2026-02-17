69c058e12b37aaf7b11ddfbd4c125d42f2d5609f
AmandlaThrishika <thrishikareddyamandla2604@gmail.com>
2026-02-17 21:27:34 +0530

    connect cloud database

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI
-import redis
+import os
 from sqlalchemy import create_engine
 
 app = FastAPI()
 
-# Redis connection
-r = redis.Redis(host="localhost", port=6379, decode_responses=True)
-
-# PostgreSQL connection
-DATABASE_URL = "postgresql://postgres:Atreddy@1516@localhost:5432/postgres"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
 @app.get("/")
 def read_root():
-    r.set("status", "Backend working!")
-    return {"message": r.get("status")}
+    return {"message": "Backend with cloud database working!"}
