25064720b6b24ef91b4e52f60989eba19ef96c8e
AmandlaThrishika <thrishikareddyamandla2604@gmail.com>
2026-02-17 21:32:57 +0530

    remove redis completely

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,9 @@ from sqlalchemy import create_engine
 app = FastAPI()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 @app.get("/")
 def read_root():
     return {"message": "Backend with cloud database working!"}
+
