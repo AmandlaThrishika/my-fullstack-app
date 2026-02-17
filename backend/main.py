34eae36bf904b964b5bf2d4da1b30c08e119f7cb
AmandlaThrishika <thrishikareddyamandla2604@gmail.com>
2026-02-17 21:38:32 +0530

    enable cors

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import os
 from sqlalchemy import create_engine
 
 app = FastAPI()
+
+# Allow frontend domain
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (safe for learning)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
 
 @app.get("/")
 def read_root():
-    return {"message": "Backend with cloud database working!"}
-
+    return {"message": "Cloud backend running!"}
